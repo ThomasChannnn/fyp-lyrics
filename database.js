d2ea9898f6956238b8591325cc2dbf9312f6fcdd
ThomasChannnn <s1154143@s.eduhk.hk>
2026-03-06 15:36:20 +0800

    march5

---
+++ database.js
@@ -0,0 +1,73 @@
+// database.js
+const songDatabase = {
+    "001": {
+        source: "Old MacDonald Had a Farm",
+        tone_answer: [4, 4, 4, 0, 2, 2, 0],
+        image: "001.png",
+        melody: "001.mp3",
+        backing: "001.mp3"
+    },
+    "002": {
+        source: "Twinkle Twinkle Little Star",
+        tone_answer: [0, 0, 4, 4, 3, 3, 4],
+        image: "002.png",
+        melody: "002.mp3",
+        backing: "002.mp3"
+    },
+    "003": {
+        source: "London Bridge is Falling Down",
+        tone_answer: [4, 3, 3, 4, 2, 4, 3],
+        image: "003.png",
+        melody: "003.mp3",
+        backing: "003.mp3"
+    },
+    "004": {
+        source: "Happy Birthday to You",
+        tone_answer: [0, 0, 2, 0, 4, 2],
+        image: "004.png",
+        melody: "004.mp3",
+        backing: "004.mp3"
+    },
+    "005": {
+        source: "We Wish You a Merry Christmas",
+        tone_answer: [0, 4, 4, 3, 4, 2, 0, 0],
+        image: "005.png",
+        melody: "005.mp3",
+        backing: "005.mp3"
+    },
+    "006": {
+        source: "Jingle Bells",
+        tone_answer: [2, 2, 2, 2, 2, 2, 2, 3, 0, 0, 2],
+        image: "006.png",
+        melody: "006.mp3",
+        backing: "006.mp3"
+    },
+    "007": {
+        source: "多啦A夢",
+        tone_answer: [0, 4, 4, 2, 3, 2, 4],
+        image: "007.png",
+        melody: "007.mp3",
+        backing: "007.mp3"
+    },
+    "008": {
+        source: "It's a Small World",
+        tone_answer: [4, 4, 3, 4, 3, 3, 3],
+        image: "008.png",
+        melody: "008.mp3",
+        backing: "008.mp3"
+    },
+    "009": {
+        source: "Lonely Christmas",
+        tone_answer: [2, 4, 2, 4, 4, 3],
+        image: "009.png",
+        melody: "009.mp3",
+        backing: "009.mp3"
+    },
+    "010": {
+        source: "Greensleeves",
+        tone_answer: [0, 4, 4, 3, 3, 3, 4, 2, 0],
+        image: "010.png",
+        melody: "010.mp3",
+        backing: "010.mp3"
+    }
+};
